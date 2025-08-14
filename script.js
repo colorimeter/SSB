9af6215f83b93e0d1472a9b46cfc122f5eab63f9
colorimeter <48435057+colorimeter@users.noreply.github.com>
2025-08-14 07:10:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const eredmenyKontener1 = document.getElementById('output1');
+    const eredmenyKontener2 = document.getElementById('output2');
+
+    const drawdata = async () => {
+
+        const apiUrl = 'https://www3.vvs.de/vvs/widget/XML_DM_REQUEST?locationServerActive=1&lsShowTrainsExplicit=1&stateless=1&language=de&SpEncId=0&anySigWhenPerfectNoOtherMatches=1&limit=10&depArr=departure&type_dm=any&anyObjFilter_dm=2&deleteAssignedStops=1&name_dm=de%3A08111%3A79&mode=direct&dmLineSelectionAll=1&useRealtime=1&outputFormat=json';
+
+        try {
+            const valasz = await fetch(apiUrl);
+            if (!valasz.ok) {
+                throw new Error(`HTTP error! ${valasz.status}`);
+            }
+
+            const resp = await valasz.json();
+
+            const varosba = resp.departureList.filter(item => item.platform === "1");
+            const kifele = resp.departureList.filter(item => item.platform === "2");
+
+            const varosba_str = varosba.map(item => {
+             const c=item.countdown.padStart(3, ' ');
+             const h=item.dateTime.hour.padStart(2, '0');
+             const m=item.dateTime.minute.padStart(2, '0');
+             return `${c} perc múlva (${h}:${m}) -> ${item.servingLine.direction}`;
+              });
+            const kifele_str = kifele.map(item => `${item.countdown} perc múlva (${item.dateTime.hour}:${item.dateTime.minute}) -> ${item.servingLine.direction}`);
+
+
+            eredmenyKontener1.textContent = varosba_str.join('\n');
+            eredmenyKontener2.textContent = kifele_str.join('\n');
+
+        } catch (e) {
+            console.error('Fetch error:', e);
+            eredmenyKontener1.innerHTML = `<p style="color: red;">ERROR: ${e.message}</p>`;
+            eredmenyKontener2.innerHTML = ''; 
+        }
+    };
+
+    drawdata();
+    const gomb = document.getElementById('adatok-lekerese');
+    gomb.addEventListener('click', drawdata);
+});
