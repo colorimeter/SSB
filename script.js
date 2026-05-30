bc984301e46ef308b4a9cf4d7bd207f537bb9c7f
colorimeter <48435057+colorimeter@users.noreply.github.com>
2026-05-30 22:18:33 +0200

    Add Rotebühl

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const eredmenyKontener1 = document.getElementById('output1');
     const eredmenyKontener2 = document.getElementById('output2');
     const eredmenyKontener3 = document.getElementById('output3');
+	const eredmenyKontener4 = document.getElementById('output4');
 
     const adatokLekereseEsMegjelenitese = async () => {
 
@@ -18,8 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
         //const apiUrl_GB = 'https://www3.vvs.de/vvs/widget/XML_DM_REQUEST?locationServerActive=1&lsShowTrainsExplicit=1&stateless=1&language=de&SpEncId=0&anySigWhenPerfectNoOtherMatches=1&limit=10&depArr=departure&type_dm=any&anyObjFilter_dm=2&deleteAssignedStops=1&name_dm=de%3A08111%3A79&mode=direct&dmLineSelectionAll=1&useRealtime=1&outputFormat=json';
         //const apiUrl_SG = 'https://www3.vvs.de/vvs/widget/XML_DM_REQUEST?locationServerActive=1&lsShowTrainsExplicit=1&stateless=1&language=de&SpEncId=0&anySigWhenPerfectNoOtherMatches=1&limit=40&depArr=departure&type_dm=any&anyObjFilter_dm=2&deleteAssignedStops=1&name_dm=de%3A08111%3A6024&mode=direct&dmLineSelectionAll=1&useRealtime=1&outputFormat=json'
 		//const apiUrl_SM = 'https://www3.vvs.de/vvs/widget/XML_DM_REQUEST?locationServerActive=1&lsShowTrainsExplicit=1&stateless=1&language=de&SpEncId=0&anySigWhenPerfectNoOtherMatches=1&limit=20&depArr=departure&type_dm=any&anyObjFilter_dm=2&deleteAssignedStops=1&name_dm=de%3A08111%3A6056%3A2%3A1&mode=direct&dmLineSelectionAll=1&useRealtime=1&outputFormat=json'
+		// Gaisburg:
 		const apiUrl_GB = api_prefix + '&limit=10&name_dm=de%3A08111%3A79';
+		// Staatsgalerie:
 		const apiUrl_SG = api_prefix + '&limit=40&name_dm=de%3A08111%3A6024';
+		// Stadtmitte (Rotebühlplatz):
 		const apiUrl_SM = api_prefix + '&limit=20&name_dm=de%3A08111%3A6056%3A2%3A1';
 
         try {
@@ -33,6 +37,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 throw new Error(`HTTP error: ${valasz_SG.status}`);
             }
 
+			const valasz_SM = await fetch(apiUrl_SM);
+            if (!valasz_SM.ok) {
+                throw new Error(`HTTP error: ${valasz_SM.status}`);
+            }
+
+			/// Gaisburg
             var resp = await valasz_GB.json();
 
             const varosba = resp.departureList.filter(item => item.platform === "1");
@@ -44,14 +54,20 @@ document.addEventListener('DOMContentLoaded', () => {
             eredmenyKontener1.textContent = varosba_str.join('\n');
             eredmenyKontener2.textContent = kifele_str.join('\n');
 
-	    resp = await valasz_SG.json();
-	    //const haza = resp.departureList.filter(item => item.servingLine.symbol === "U4" );
-	    const haza = resp.departureList.filter(item => item.servingLine.symbol === "U4" && item.servingLine.direction.includes("Untertürkheim"));
+			/// Staatsgalerie
+	   		resp = await valasz_SG.json();
+	   	    const haza = resp.departureList.filter(item => item.servingLine.symbol === "U4" && item.servingLine.direction.includes("Untertürkheim"));
             const haza_str = haza.map(item => msgformat(item.countdown,item.dateTime.hour,item.dateTime.minute,item.servingLine.direction));
 
             eredmenyKontener3.textContent = haza_str.join('\n');
 
+            /// Stadtmitte
+			resp = await valasz_SM.json();
+			const hazasm = resp.departureList.filter(item => item.servingLine.symbol === "U4" && item.servingLine.direction.includes("Untertürkheim"));
+            const hazasm_str = hazasm.map(item => msgformat(item.countdown,item.dateTime.hour,item.dateTime.minute,item.servingLine.direction));
+            eredmenyKontener4.textContent = hazasm_str.join('\n');
 
+			
         } catch (hiba) {
             console.error('Hiba történt a lekérés során:', hiba);
             eredmenyKontener1.innerHTML = `<p style="color: red;">Hiba: ${hiba.message}</p>`;
